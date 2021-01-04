465a0fedf0734c6ae9d118b1f5d00d725b33df42
defoebrand <defoe.brand@gmail.com>
2021-01-04 13:40:02 +0200

    Set up project directory structure

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import BooksList from '../containers/BooksList';
+import BooksForm from '../containers/BooksForm';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BooksList />
+      <BooksForm />
+    </div>
+  );
+}
+
+export default App;
