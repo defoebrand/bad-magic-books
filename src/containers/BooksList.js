47ffb5693a05b6ccb0b16bc558a50b0798d0c07e
defoebrand <defoe.brand@gmail.com>
2021-01-04 14:23:37 +0200

    Implement BooksList

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,12 +1,30 @@
-import BooksForm from './BooksForm';
-import '../styles/App.css';
-
-function BooksList() {
+export default function BooksList() {
+  // const { bookID, title, category } = props;
+  const books = [
+    {
+      bookID: 0,
+      title: 'To Kill a Mockingbird',
+      category: 'Southern Gothic Fiction',
+    }, {
+      bookID: 1,
+      title: 'Drawing Blood',
+      category: 'Horror',
+    },
+  ];
   return (
-    <div className="App">
-      <BooksForm />
-    </div>
+    <table>
+      <tr>
+        <th>Book ID</th>
+        <th>Title</th>
+        <th>Category</th>
+      </tr>
+      {books.map(value => (
+        <tr key={value}>
+          <td>{value.bookID}</td>
+          <td>{value.title}</td>
+          <td>{value.category}</td>
+        </tr>
+      ))}
+    </table>
   );
 }
-
-export default BooksList;
